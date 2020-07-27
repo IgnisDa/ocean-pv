ae6f738322eb3475ed43a897287ab8744328bbd5
IgnisDa <diptesh.choudhuri@gmail.com>
2020-07-28 00:21:56 +0530

    update settings

--- ocean_website/settings/base.py
+++ ocean_website/settings/base.py
@@ -91,6 +91,7 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
