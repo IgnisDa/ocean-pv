18813c081dc66dd0a8abf6d0189974c4d7a65bf8
IgnisDa <diptesh.choudhuri@gmail.com>
2020-07-28 12:57:17 +0530

    update settings

--- ocean_website/settings/heroku.py
+++ ocean_website/settings/heroku.py
@@ -8,13 +8,13 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_PORT = 587
 
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
--- ocean_website/wsgi.py
+++ ocean_website/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'ocean_website.settings.production')
+                      'ocean_website.settings.heroku')
 
 application = get_wsgi_application()
