bd6a2e837fc652e4c9e4460be4262863fbf7b1ba
IgnisDa <diptesh.choudhuri@gmail.com>
2020-07-28 13:55:13 +0530

    update Procfile

--- ocean_website/settings/heroku.py
+++ ocean_website/settings/heroku.py
@@ -4,7 +4,7 @@ from .base import *  # NOQA
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '0.0.0.0']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -23,3 +23,20 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY')
 
 GOOGLE_RECAPTCHA_SITE_KEY = os.environ.get('GOOGLE_RECAPTCHA_SITE_KEY')
+
+DATABASE_NAME = os.environ.get('DATABASE_NAME')
+
+DATABASE_USER = os.environ.get('DATABASE_USER')
+
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- ocean_website/wsgi.py
+++ ocean_website/wsgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'ocean_website.settings.heroku')
 
